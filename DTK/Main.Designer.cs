cab316441a75d27d27997f16d31597274adced89
Justyn <justync9@gmail.com>
2016-04-16 14:16:39 -0500

    Begin work on reading from file

--- DTK/Main.Designer.cs
+++ DTK/Main.Designer.cs
@@ -37,7 +37,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.locationBox = new System.Windows.Forms.TextBox();
             this.browseButton = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.titleView = new System.Windows.Forms.ListView();
             this.titleName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.titleID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.titleKey = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -65,27 +65,28 @@
             this.browseButton.TabIndex = 2;
             this.browseButton.Text = "Browse";
             this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
             // 
-            // listView1
+            // titleView
             // 
-            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.titleView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.titleView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.titleName,
             this.titleID,
             this.titleKey,
             this.region,
             this.isLegit});
-            this.listView1.GridLines = true;
-            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            this.titleView.GridLines = true;
+            this.titleView.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
             listViewItem1});
-            this.listView1.Location = new System.Drawing.Point(12, 38);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(541, 211);
-            this.listView1.TabIndex = 3;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.titleView.Location = new System.Drawing.Point(12, 38);
+            this.titleView.Name = "titleView";
+            this.titleView.Size = new System.Drawing.Size(541, 211);
+            this.titleView.TabIndex = 3;
+            this.titleView.UseCompatibleStateImageBehavior = false;
+            this.titleView.View = System.Windows.Forms.View.Details;
             // 
             // titleName
             // 
@@ -117,7 +118,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(565, 261);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.titleView);
             this.Controls.Add(this.browseButton);
             this.Controls.Add(this.locationBox);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -131,7 +132,7 @@
         #endregion
         private System.Windows.Forms.TextBox locationBox;
         private System.Windows.Forms.Button browseButton;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView titleView;
         private System.Windows.Forms.ColumnHeader titleName;
         private System.Windows.Forms.ColumnHeader titleID;
         private System.Windows.Forms.ColumnHeader titleKey;
--- DTK/Main.cs
+++ DTK/Main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,61 @@ namespace DTK
         public Main()
         {
             InitializeComponent();
+            titleView.Items.Clear();
+        }
+
+        private static Dictionary<string, string> ParseDecTitleKeysBin(String DecryptedTitleKeysPath)
+        {
+            var ticketsDictionary = new Dictionary<string, string>();
+
+            using (var reader = new BinaryReader(new FileStream(DecryptedTitleKeysPath, FileMode.Open, FileAccess.Read)))
+            {
+                var numberOfKeys = new FileInfo(DecryptedTitleKeysPath).Length / 32;
+
+                reader.ReadBytes(16); // seek in
+
+                for (var entry = 0; entry < numberOfKeys; entry++)
+                {
+                    reader.ReadBytes(8); // skip 8 bytes
+                    var titleId = BitConverter.ToString(reader.ReadBytes(8)).Replace("-", string.Empty);
+                    var titleKey = BitConverter.ToString(reader.ReadBytes(16)).Replace("-", string.Empty);
+
+                    // var pair = BitConverter.ToString(titleId) + ": " + BitConverter.ToString(titleKey);
+                    // tickets.Add(pair);
+                    ticketsDictionary[titleId] = titleKey;
+                }
+            }
+
+            return ticketsDictionary;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            // Displays an OpenFileDialog so the user can select a Cursor.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "BIN files|*.bin";
+            openFileDialog1.Title = "Select a decTitleKeys.bin File";
+
+            // Show the Dialog.
+            // If the user clicked OK in the dialog and
+            // a .CUR file was selected, open it.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Assign the cursor in the Stream to the Form's Cursor property.
+                this.locationBox.Text = openFileDialog1.FileName;
+                this.titleView.Items.Clear();
+                Dictionary<string, string> titleDic = ParseDecTitleKeysBin(openFileDialog1.FileName);
+
+                foreach (KeyValuePair<string, string> entry in myDic)
+                {
+                    
+                }
+            }
         }
     }
 }
